3e18c189261a9960d32836e38cb7843bdc21914f
sharyu <sharyugore.17@gmail.com>
2021-05-09 02:35:09 +0530

    Exception Handling and Internationalization

--- src/main/java/com/sharyu/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/sharyu/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,13 +1,26 @@
 package com.sharyu.restfulwebservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
+@EnableWebMvc
 public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver slr = new AcceptHeaderLocaleResolver();
+		slr.setDefaultLocale(Locale.ENGLISH);
+		return slr;
+	}
 }
